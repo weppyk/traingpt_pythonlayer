b57f815caf93bcecc713a8d58a1a6cbd16769882
Radek Drlík <radek.drlik@gmail.com>
2023-05-05 20:09:34 +0200

    connection with chatgpt

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a virtual environment for the app
+#python -m venv env
+
+# Activate the virtual environment
+#source env/bin/activate
+
+# Install the required Python packages
+pip install -r requirements.txt
+
+# Set up the OpenAI API key as an environment variable
+#export OPENAI_API_KEY="YOUR_OPENAI_API_KEY_HERE"
+
+# Deactivate the virtual environment
+#deactivate
